f78eee62f5a3db77ed99adc45e8086e30b0acf92
Intanjeno02 <jenjenleeo23@gmail.com>
2026-03-31 14:53:54 +0700

    pretest modul 4

---
+++ Pretest/pretest/src/Mahasiswa.java
@@ -0,0 +1,17 @@
+class Mahasiswa {
+    String nama;
+    int umur;
+    String Jurusan;
+
+    Public Mahasiswa (String nama, int umur, String Jurusan){
+        this.nama = nama;
+        this.umur = umur;
+        this.Jurusan = Jurusan;
+    }
+}
+
+Public class main {
+    public static void main(String[] args){
+        Mahasiswa mhs = new Mahasiswa(nama: "Intan Rhevita", umur: 19, Jurusan: "Ilmu Komputer")
+    }
+}
