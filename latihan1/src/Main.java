8de64827adebee03aa3b0af714b68ea8b9c695b5
Intanjeno02 <jenjenleeo23@gmail.com>
2026-03-31 16:25:59 +0700

    latihan praktikum 1-4

--- latihan1/src/Main.java
+++ latihan1/src/Main.java
@@ -1,5 +1,12 @@
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+public class Main {
+    public static void main(String[] args){
+        MesinKopi mesinLobby = new MesinKopi (18.5 , 15, 15 );
+        mesinLobby.Berhasil();
+        mesinLobby.isiUlangBahan(15, 15, 15);
+        mesinLobby.Berhasil();
+
+        boolean Ketersediaan = mesinLobby.cekKetersediaanCappuccino();
+        System.out.println("pengecekan cappucino :" + Ketersediaan);
+        
     }
 }
--- latihan1/src/MesinKopi.java
+++ latihan1/src/MesinKopi.java
@@ -1,6 +1,39 @@
 class MesinKopi {
-    Double bijikopi;
-    Double air;
-    Double susu;
+    double bijikopi;
+    double air;
+    double susu;
 
+    public MesinKopi(double bijikopi, double air, double susu) {
+        System.out.println("bahan baku sebelum diisi ulang.");
+        this.bijikopi = bijikopi;
+        this.air = air;
+        this.susu = susu;
+    }
+
+    public void isiUlangBahan(double bijikopi, double air, double susu) {
+        this.bijikopi = bijikopi + this.bijikopi;
+        this.air = air + this.air;
+        this.susu = susu + this.susu;
+        System.out.println("bahan baku berhasil diisi ulang.");
+    }
+
+    public void Berhasil(){
+        System.out.println("biji kopi :" + this.bijikopi);
+        System.out.println("air :" + this.air);
+        System.out.println("susu :" + this.susu);
+    }
+
+    boolean cekKetersediaanCappuccino(){
+        if (bijikopi >= 15.0 && air >= 50.0 && susu >= 100.0){
+            return true;
+        } else{
+            return false;
+        }
+    }
+
+    MesinKopi(){
+        bijikopi = 0.0;
+        air = 0.0;
+        susu = 0.0;
+    }
 }
